62ec4440d836f5d1fd6407a5c72614e8c0632fdc
benHung1 <ga2006206521@gmail.com>
2022-01-14 16:43:30 +0800

    YEL

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,9 +8,9 @@ const Hero = () => {
     <HeroSection>
       <HeroVideo src="https://vod-progressive.akamaized.net/exp=1642163748~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F789%2F7%2F178947119%2F583336002.mp4~hmac=123ede40ae00caf2807dc6648ae02a7deb4113d576e9d83149025f3b18ba9f35/vimeo-prod-skyfire-std-us/01/789/7/178947119/583336002.mp4?filename=Animation+Of+Digital+Human+Face.mp4" autoPlay loop muted/>
       <Container>
-        <MainHeading>Your data is secure with us</MainHeading>
+        <MainHeading>Your Data Is Secure With Us</MainHeading>
         <HeroText>
-          We provide the best security system for clients all over the world
+          We Provide The Best Security System For Clients All Over The World
         </HeroText>
         <ButtonWrapper>
           <Link to ="signup">

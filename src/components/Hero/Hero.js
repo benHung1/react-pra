9c4e5bfa456fb7ed089d5e381d7a95b1c3a4bd10
benHung1 <ga2006206521@gmail.com>
2022-01-14 16:42:02 +0800

    okayy

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,7 +6,7 @@ import { HeroVideo, HeroSection, HeroText, ButtonWrapper, HeroButton } from './H
 const Hero = () => {
   return (
     <HeroSection>
-      <HeroVideo src="./assets/hero.mp4" autoPlay muted/>
+      <HeroVideo src="https://vod-progressive.akamaized.net/exp=1642163748~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F789%2F7%2F178947119%2F583336002.mp4~hmac=123ede40ae00caf2807dc6648ae02a7deb4113d576e9d83149025f3b18ba9f35/vimeo-prod-skyfire-std-us/01/789/7/178947119/583336002.mp4?filename=Animation+Of+Digital+Human+Face.mp4" autoPlay loop muted/>
       <Container>
         <MainHeading>Your data is secure with us</MainHeading>
         <HeroText>
